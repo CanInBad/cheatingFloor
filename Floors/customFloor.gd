1884b32824ea106665d46f70e5b67e343dd910bb
CanInBad <73659199+CanInBad@users.noreply.github.com>
2024-01-28 18:11:19 +0700

    I did not know that it would do this

--- Floors/customFloor.gd
+++ Floors/customFloor.gd
@@ -1,1 +1,12 @@
-../../../Game/World/Floors/customFloor.gd
+extends SubGameWorld
+
+
+
+# func _on_CheatingRoom_onEnter(room):
+# 	room.addButton("Leave", "Return to the prison", "leave")
+
+
+# func _on_CheatingRoom_onReact(_room, key):
+# 	if(key == "leave"):
+# 		GM.pc.setLocation("yard_deadend1")
+# 		GM.main.reRun()
