bd0df712c88133efec221aac087954da1de8c655
CanInBad <73659199+CanInBad@users.noreply.github.com>
2024-01-28 18:23:16 +0700

    I give up.

---
+++ Floor/customFloor.gd
@@ -0,0 +1,12 @@
+extends SubGameWorld
+
+
+
+# func _on_CheatingRoom_onEnter(room):
+# 	room.addButton("Leave", "Return to the prison", "leave")
+
+
+# func _on_CheatingRoom_onReact(_room, key):
+# 	if(key == "leave"):
+# 		GM.pc.setLocation("yard_deadend1")
+# 		GM.main.reRun()
