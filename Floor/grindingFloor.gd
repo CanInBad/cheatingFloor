35b41a4ca702753063e8e0a5ba3a0189a1cc18de
CanInBad <73659199+CanInBad@users.noreply.github.com>
2024-01-29 01:45:39 +0700

    2 am energy is really high

--- Floor/grindingFloor.gd
+++ Floor/grindingFloor.gd
@@ -2,11 +2,13 @@ extends SubGameWorld
 
 
 
-# func _on_CheatingRoom_onEnter(room):
-# 	room.addButton("Leave", "Return to the prison", "leave")
+func _on_grindingRoomCIB_enter(_room):
+    GM.main.playAnimation(StageScene.Duo, "stand", {npc="caninbad", npcAction="sit"})
+	# room.addButton("Leave", "Return to the prison", "leave")
 
 
-# func _on_CheatingRoom_onReact(_room, key):
-# 	if(key == "leave"):
-# 		GM.pc.setLocation("yard_deadend1")
-# 		GM.main.reRun()
+func _on_grindingRoomCIB_react(_room,_key):
+    pass
+	# if(key == "leave"):
+	# 	GM.pc.setLocation("yard_deadend1")
+	# 	GM.main.reRun()
--- Module.gd
+++ Module.gd
@@ -10,10 +10,35 @@ func _init():
 	author = "CanInBad"
 
 	events = [
-		"res://Modules/Y_grindingFloor/Events/EntranceEnter.gd",
-		"res://Modules/Y_grindingFloor/Events/EntranceLeave.gd",
+		"res://Modules/Z_IgrindingFloor/Events/EntranceEnter.gd",
+		"res://Modules/Z_IgrindingFloor/Events/EntranceLeave.gd",
 	]
 	scenes = [
-		"res://Modules/Y_grindingFloor/Scenes/discoverHatch.gd"
+		"res://Modules/Z_IgrindingFloor/Scenes/discoverHatch.gd"
 	]
+	characters = []
+	if checkMods():
+		characters.append("res://Modules/Z_IgrindingFloor/CIBcharModded.gd")
+	else:
+		characters.append("res://Modules/Z_IgrindingFloor/CIBcharNonModded.gd")
+
+
+func checkMods(): # will return true if all mods present
+	var beutifiedList = {
+		"Dragons+": "Dragon+", 
+		"DEV BITS": "ScalieExpandsion",
+		"Hypertus": "Hypertus",
+		"SynthSpecies": "Synth Species",
+		"Knotted Horse Cock": "KnottedEquinePenis",
+	}
+	var notPresent:Array = []
+	for i in beutifiedList.keys():
+		if !(i in GlobalRegistry.getModules()):
+			notPresent.append(i)
 	
+	if len(notPresent)>0:
+		Log.printerr("### "+id+": Atleast a mod is missing for custom character!")
+		for i in notPresent:
+			Log.print("### "+id+": Missing "+ beutifiedList[i])
+		return false
+	return true
